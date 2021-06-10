e51b8a0c8d949186e34bd1fd601b448ad2c313e4
Mariam  Khamo <mariamw736@gmail.com>
2021-06-10 20:53:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
-fetch('https://anapioficeandfire.com/api/books')
+  fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(data => renderBooks(data));
+
+}
